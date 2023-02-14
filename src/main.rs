80aece25ab2a7795ebd3d91d23de9f4d35483ca9
Filip Grabowski <grabfilip2007@gmail.com>
2023-02-14 22:46:04 +0100

    Added get all guitars method

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,31 @@
-use actix_web::{get, post, web, HttpResponse, HttpServer, Responder, App};
+use std::fs;
+use actix_web::{get, post, web::{self}, HttpResponse, HttpServer, Responder, App, Result, guard::Get};
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn get_all() -> Result<impl Responder> {
+    let data = fs::read_to_string("src/data.json")
+        .expect("Unable to read file");
+    let json: serde_json::Value = serde_json::from_str(&data)
+        .expect("JSON does not have correct format.");
+    Ok(web::Json(json))
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
+// #[get("/{guitar_id}")]
+// async fn get_guitar(guitar_id: web::Path<u32>) -> Result<impl Responder> {
+//     let data = fs::read_to_string("src/data.json")
+//         .expect("Unable to read file");
+//     let json: serde_json::Value = serde_json::from_str(&data)
+//         .expect("JSON does not have correct format.");
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
+//     let v = json!()
+//     Ok(web::Json(json))
+// }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))  
+            .service(get_all)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
