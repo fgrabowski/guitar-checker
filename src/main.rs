899b8c97fc9736308bf832f6f3dbf92da9d472f6
Filip Grabowski <grabfilip2007@gmail.com>
2023-02-14 21:30:39 +0100

    inital commit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, HttpResponse, HttpServer, Responder, App};
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -12,4 +12,17 @@ async fn echo(req_body: String) -> impl Responder {
 
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))  
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
