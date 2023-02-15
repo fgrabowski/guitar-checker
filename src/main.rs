be9a47917fec9cc53e1b46bdf9318bf3fc752168
gbaranski <me@gbaranski.com>
2023-02-15 19:38:29 +0100

    style: reformat code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use actix_web::{
+    error, get, post,
+    web::{self},
+    App, HttpServer, Responder, Result,
+};
+use serde::{Deserialize, Serialize};
 use std::fs;
-use actix_web::{get, web::{self}, HttpServer, Responder, App, Result, error, post};
-use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Wood {
@@ -10,7 +14,7 @@ struct Wood {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct  Guitar {
+struct Guitar {
     id: String,
     model: String,
     construction: String,
@@ -20,26 +24,27 @@ struct  Guitar {
 
 #[get("/")]
 async fn get_all() -> Result<impl Responder> {
-    let data = fs::read_to_string("src/data.json")
-        .expect("Unable to read file");
-    let json: serde_json::Value = serde_json::from_str(&data)
-        .expect("JSON does not have correct format.");
+    let data = fs::read_to_string("src/data.json").expect("Unable to read file");
+    let json: serde_json::Value =
+        serde_json::from_str(&data).expect("JSON does not have correct format.");
     Ok(web::Json(json))
 }
 
 #[get("/{guitar_id}")]
 async fn get_guitar(guitar_id: web::Path<String>) -> Result<impl Responder> {
-    let data = fs::read_to_string("src/data.json")
-        .expect("Unable to read file");
+    let data = fs::read_to_string("src/data.json").expect("Unable to read file");
 
     let deserialized: Vec<Guitar> = serde_json::from_str(&data).unwrap();
 
-    let matching_guitar = deserialized.into_iter().find(|g| g.id == guitar_id.to_string());
-    
+    let matching_guitar = deserialized
+        .into_iter()
+        .find(|g| g.id == guitar_id.to_string());
 
     match matching_guitar {
         Some(matching_guitar) => Ok(web::Json(matching_guitar)),
-        None => Err(error::ErrorNotFound("Error: Guitar with specified ID doesn't exist.")),
+        None => Err(error::ErrorNotFound(
+            "Error: Guitar with specified ID doesn't exist.",
+        )),
     }
 }
 
